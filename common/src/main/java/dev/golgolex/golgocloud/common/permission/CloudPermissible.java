9cd056091e565a5cac3a0ecd586cd9b0dd47523c
Pascal <75942385+TVGolgolex@users.noreply.github.com>
2024-05-27 20:30:26 +0200

    Enhanced permission mechanism and implemented BufferClass interface
    New classes `CloudPermissible` and `Permission` were introduced to optimize permission management. Both classes now implement the BufferClass interface to facilitate data serialization for efficient networking. Unnecessary fields were also removed to improve code readability and performance.

--- common/src/main/java/dev/golgolex/golgocloud/common/permission/CloudPermissible.java
+++ common/src/main/java/dev/golgolex/golgocloud/common/permission/CloudPermissible.java
@@ -1,9 +1,13 @@
 package dev.golgolex.golgocloud.common.permission;
 
 import dev.golgolex.quala.netty5.protocol.buffer.BufferClass;
+import dev.golgolex.quala.netty5.protocol.buffer.CodecBuffer;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -11,6 +15,21 @@ import java.util.UUID;
 public abstract class CloudPermissible implements BufferClass {
     
     private UUID id;
-    private Lis
+    private List<Permission> permissions = new ArrayList<>();
 
+    @Override
+    public void writeBuffer(@NotNull CodecBuffer codecBuffer) {
+        codecBuffer.writeUniqueId(id);
+        codecBuffer.writeList(permissions, (it, permission) -> permission.writeBuffer(it));
+    }
+
+    @Override
+    public void readBuffer(@NotNull CodecBuffer codecBuffer) {
+        this.id = codecBuffer.readUniqueId();
+        this.permissions = codecBuffer.readList(new ArrayList<>(), () -> {
+            var permission = new Permission();
+            permission.readBuffer(codecBuffer);
+            return permission;
+        });
+    }
 }
--- common/src/main/java/dev/golgolex/golgocloud/common/permission/Permission.java
+++ common/src/main/java/dev/golgolex/golgocloud/common/permission/Permission.java
@@ -1,11 +1,24 @@
 package dev.golgolex.golgocloud.common.permission;
 
+import dev.golgolex.quala.netty5.protocol.buffer.BufferClass;
+import dev.golgolex.quala.netty5.protocol.buffer.CodecBuffer;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
 
 @Getter
 @Accessors(fluent = true)
-public class Permission {
+public class Permission implements BufferClass {
     private String value;
     private boolean state;
+
+    @Override
+    public void writeBuffer(@NotNull CodecBuffer codecBuffer) {
+        
+    }
+
+    @Override
+    public void readBuffer(@NotNull CodecBuffer codecBuffer) {
+
+    }
 }
