dd3a73c556de036b4059e9e52f4fc5002e976df4
Pascal <75942385+TVGolgolex@users.noreply.github.com>
2024-05-30 15:28:25 +0200

    Add CloudPlayer packet receivers
    Created new 'CloudPlayerCreatePacketReceiver' class and updated 'CloudNetworkProviderImpl' to initialize packet handlers for user's login and creation events. This update enables the system to process and handle these incoming packets appropriately.

--- api/src/main/java/dev/golgolex/golgocloud/cloudapi/network/CloudNetworkProviderImpl.java
+++ api/src/main/java/dev/golgolex/golgocloud/cloudapi/network/CloudNetworkProviderImpl.java
@@ -1,8 +1,12 @@
 package dev.golgolex.golgocloud.cloudapi.network;
 
 import dev.golgolex.golgocloud.cloudapi.service.packets.CloudServiceUpdatePacketReceiver;
+import dev.golgolex.golgocloud.cloudapi.user.packets.CloudPlayerCreatePacketReceiver;
+import dev.golgolex.golgocloud.cloudapi.user.packets.CloudPlayerLoginPacketReceiver;
 import dev.golgolex.golgocloud.common.network.CloudNetworkProvider;
 import dev.golgolex.golgocloud.common.service.packets.CloudServiceUpdatePacket;
+import dev.golgolex.golgocloud.common.user.packets.CloudPlayerCreatePacket;
+import dev.golgolex.golgocloud.common.user.packets.CloudPlayerLoginPacket;
 import dev.golgolex.quala.netty5.protocol.receiver.PacketReceiverManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,5 +15,9 @@ public class CloudNetworkProviderImpl implements CloudNetworkProvider {
     public void initPacketReceivers(@NotNull PacketReceiverManager registry) {
         // service packets
         registry.registerPacketHandler(CloudServiceUpdatePacket.class, CloudServiceUpdatePacketReceiver.class);
+
+        // user packets
+        registry.registerPacketHandler(CloudPlayerLoginPacket.class, CloudPlayerLoginPacketReceiver.class);
+        registry.registerPacketHandler(CloudPlayerCreatePacket.class, CloudPlayerCreatePacketReceiver.class);
     }
 }
---
+++ api/src/main/java/dev/golgolex/golgocloud/cloudapi/user/packets/CloudPlayerCreatePacketReceiver.java
@@ -0,0 +1,13 @@
+package dev.golgolex.golgocloud.cloudapi.user.packets;
+
+import dev.golgolex.golgocloud.cloudapi.CloudAPI;
+import dev.golgolex.golgocloud.common.user.packets.CloudPlayerCreatePacket;
+import dev.golgolex.quala.netty5.channel.NetworkChannel;
+import dev.golgolex.quala.netty5.protocol.receiver.PacketReceiver;
+
+public class CloudPlayerCreatePacketReceiver extends PacketReceiver<CloudPlayerCreatePacket> {
+    @Override
+    public void receivePacket(CloudPlayerCreatePacket packet, NetworkChannel networkChannel) {
+        CloudAPI.instance().cloudPlayerProvider().putCache(packet.cloudPlayer());
+    }
+}
